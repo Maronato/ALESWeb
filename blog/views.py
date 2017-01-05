124c918013cd6f1bbe0ca8f2e795738a6e4895b7
Rafael Goncalves <rafaelgoncalves258@gmail.com>
2017-01-05 15:11:46 -0200

    created blog template, views and urls. Added blog/urls to project/urls

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from blog.models import Post
+
+def blog(request):
+    all_posts = Post.objects.all().order_by('-date')
+    data = {'posts': all_posts,}
+
+    return render(request, 'blog/index.html', data)
 
 # Create your views here.
