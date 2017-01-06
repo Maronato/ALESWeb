69a1eba26e55812bfc84d67d576f559576be6e02
Rafael Goncalves <rafaelgoncalves258@gmail.com>
2017-01-06 12:04:09 -0200

    created template for blog/detail

--- blog/views.py
+++ blog/views.py
@@ -3,11 +3,19 @@ from blog.models import Post
 
 
 def index(request):
-    all_posts = Post.objects.all().order_by('-date')
-    return render(request, 'blog/index.html', {'posts': all_posts,})
+    all_posts = Post.objects.all().order_by('-created')
+    if len(all_posts) >= 5:
+        posts = all_posts[:5]
+    else:
+        posts = all_posts
+    return render(request, 'blog/index.html', {
+    'posts': posts,
+    })
     
     
 def detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
-    return render(request, 'blog/detail.html', {'post' = post)
+    return render(request, 'blog/detail.html', {
+        'post': post,
+        })
 
