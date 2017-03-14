ab7b062b8035a0cd474ea32075edf8cace4815ff
Maronato <gustavomaronato@gmail.com>
2017-03-14 19:36:43 -0300

    made it easier to instantiate AllEvents

--- courses/date_comparisons.py
+++ courses/date_comparisons.py
@@ -3,21 +3,26 @@ from dateutil.relativedelta import relativedelta
 
 class AllEvents():
 
-    def __init__(self, begin, weeks=0, months=0):
-        self.begin = begin
-        self.args = {}
-        if weeks != 0:
-            self.args['weeks'] = +weeks
-        if months != 0:
-            self.args['months'] = +months
+    def __init__(self, begin=None, weeks=0, months=0, course=None):
+        if course:
+            self.begin = course.begin
+            self.weeks = course.weeks_apart
+            self.months = course.months_apart
+        else:
+            self.begin = begin
+            self.args = {}
+            if weeks != 0:
+                self.args['weeks'] = +weeks
+            if months != 0:
+                self.args['months'] = +months
 
-        self.end = begin + relativedelta(months=+6)
+        self.end = self.begin + relativedelta(months=+6)
 
     def generate(self):
         current = self.begin
         while current < self.end:
             yield current
-            current = current + relativedelta(**self.args)
+            current = current + relativedelta(weeks=self.weeks, months=self.months)
 
     def compare(self, gen_2):
         for i in self.generate():
