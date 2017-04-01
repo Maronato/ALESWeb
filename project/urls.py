2a9a1448bdbfcb86d5ae8a8978b347fcb5e9467a
Maronato <gustavomaronato@gmail.com>
2017-04-01 17:04:46 -0300

    fixed template uri

--- project/urls.py
+++ project/urls.py
@@ -26,9 +26,9 @@ urlpatterns = [
     url(r'^blog/', include('blog.urls')),
 
     # Google search verification
-    url(r'^google7db2e931cb4e4f1b\.html$', TemplateView.as_view(template_name='main/google.html'), content_type='text/plain'),
+    url(r'^google7db2e931cb4e4f1b\.html$', TemplateView.as_view(template_name='main/verification/google.html'), content_type='text/plain'),
 
     # SSL verification
-    url(r'^\.well-known/acme-challenge/vteo4vSAsEH8oULL9cwcdo6WXxLgGIlkjIn7ndP8ZLI', TemplateView.as_view(template_name="main/ssl.html")),
+    url(r'^\.well-known/acme-challenge/vteo4vSAsEH8oULL9cwcdo6WXxLgGIlkjIn7ndP8ZLI', TemplateView.as_view(template_name="main/verification/ssl.html")),
 ]
 urlpatterns += main.urls.urlpatterns
