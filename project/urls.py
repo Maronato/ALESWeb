b29ad4721869139249216217b8eef281b129ac42
Maronato <gustavomaronato@gmail.com>
2017-04-01 18:48:17 -0300

    added facebook and google tags and an alt logo url

--- project/urls.py
+++ project/urls.py
@@ -27,6 +27,8 @@ urlpatterns = [
 
     # Logo
     url(r'^logo$', TemplateView.as_view(template_name='main/misc/logo.html')),
+    # Alt Logo
+    url(r'^altlogo$', TemplateView.as_view(template_name='main/misc/altlogo.html')),
 
     # Google search verification
     url(r'^google7db2e931cb4e4f1b\.html$', TemplateView.as_view(template_name='main/misc/google.html', content_type='text/plain')),
