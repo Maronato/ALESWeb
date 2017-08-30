09ede77cde9b0746bc506143f0f17f622de7d965
Maronato <gustavomaronato@gmail.com>
2017-08-30 00:30:48 -0300

    catch errors with no message

--- project/manual_error_report.py
+++ project/manual_error_report.py
@@ -6,7 +6,7 @@ from django.views.debug import ExceptionReporter
 def exception_email(request, e):
     exc_info = sys.exc_info()
     reporter = ExceptionReporter(request, is_email=True, *exc_info)
-    subject = e.message.replace('\n', '\\n').replace('\r', '\\r')[:989]
+    subject = e.message.replace('\n', '\\n').replace('\r', '\\r')[:989] if getattr(e, 'message', False) else 'Erro não identificado'
     message = reporter.get_traceback_text()
     mail.mail_admins(
         subject, message, fail_silently=False,
