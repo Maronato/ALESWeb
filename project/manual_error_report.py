b585cb58ab07be362dbb54d6f43776e43ba6c519
Maronato <gustavomaronato@gmail.com>
2017-09-15 22:20:53 -0300

    catch all errors

--- project/manual_error_report.py
+++ project/manual_error_report.py
@@ -1,14 +1,34 @@
 import sys
 from django.core import mail
 from django.views.debug import ExceptionReporter
+from django.core.mail import send_mail
+from django.conf import settings
 
 
 def exception_email(request, e):
-    exc_info = sys.exc_info()
-    reporter = ExceptionReporter(request, is_email=True, *exc_info)
-    subject = e.message.replace('\n', '\\n').replace('\r', '\\r')[:989] if getattr(e, 'message', False) else 'Erro não identificado'
-    message = reporter.get_traceback_text()
-    mail.mail_admins(
-        subject, message, fail_silently=False,
-        html_message=reporter.get_traceback_html()
-    )
+    try:
+        exc_info = sys.exc_info()
+        reporter = ExceptionReporter(request, is_email=True, *exc_info)
+        subject = e.message.replace('\n', '\\n').replace('\r', '\\r')[:989] if getattr(e, 'message', False) else 'Erro não identificado'
+        message = reporter.get_traceback_text()
+        mail.mail_admins(
+            subject, message, fail_silently=False,
+            html_message=reporter.get_traceback_html()
+        )
+    except Exception as ee:
+        to = str(settings.ADMINS[0][1])
+        fr = str(settings.DEFAULT_FROM_EMAIL)
+        try:
+            mail.mail_admins(
+                "Unexpected error " + e + " and " + ee,
+                '"' + e + '" and "' + ee + '"',
+                fail_silently=False,
+                html_message=reporter.get_traceback_html()
+            )
+        except Exception as eee:
+            send_mail(
+                'Really Unexpected Error',
+                '"' + e + '" and "' + ee + '" and "' + eee + '"',
+                fr,
+                [to],
+            )
